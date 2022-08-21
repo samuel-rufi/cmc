580ea35af875aea37d33934b16baa9ef1bb0569c
samuel_rufi <samuel.rufinatscha@iota.org>
2022-08-21 11:49:49 +0200

    Fix import

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::fs;
+use crate::entry::FullEntry;
 
 mod entry;
 
 fn main() {
     let data = fs::read_to_string("input.json").expect("Unable to read file");
     let full: FullEntry = serde_json::from_str(&data).unwrap();
-    println!("{}", full);
 
 
     // 1. Open CMC page
